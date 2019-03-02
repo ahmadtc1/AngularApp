bdaf40b7c80cc39a50013fd022934e88532a4598
Your Name <you@example.com>
2019-03-02 18:47:19 -0500

    adding http request for data

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct }  from './product';
-
+import { ProductService } from './product.service'
 
 @Component({
     selector: 'pm-products',
@@ -15,6 +15,7 @@ export class ProductListComponent implements OnInit{
     showImage: boolean = false;
     showPrice: boolean = false;
     _listFilter: string;
+    errorMessage:string;
 
     get listFilter(): string {
         return this._listFilter
@@ -26,28 +27,7 @@ export class ProductListComponent implements OnInit{
     }
 
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2016",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-        }
-    ];
+    products: IProduct[] = [];
 
     constructor() {
         this.filteredProducts = this.products;
@@ -68,7 +48,10 @@ export class ProductListComponent implements OnInit{
     } 
 
     ngOnInit(): void {
-        console.log('In OnInit')
+        this.productService.getProducts().subscribe(
+            products => this.products = products,
+            error => this.errorMessage = <any> error
+        );
     }
 
     onRatingClicked(message: string): void {
