7ad265dab95f858c0aef9abf960be166a9940003
Your Name <you@example.com>
2019-03-07 21:17:07 -0500

    fixing module imports

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,31 +1,28 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
-import { StarComponent } from '../shared/star.component';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ProductDetailGuard } from './product-detail.guard';
+import { SharedModule } from '../shared/shared.module';
+import { ConvertFirstLetterUppercase } from '../shared/convert-first-letter-uppercase.pipe.pipe';
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
-      {path: 'products', component: ProductListComponent },
+      { path: 'products', component: ProductListComponent },
       {
         path: 'products/:id',
         canActivate: [ProductDetailGuard],
-        component: ProductDetailComponent 
+        component: ProductDetailComponent
       },
-    ])
+    ]),
+    SharedModule
   ],
   declarations: [
     ProductListComponent,
-    ProductDetailComponent, 
+    ProductDetailComponent,
     ConvertToSpacesPipe,
-    StarComponent,
   ]
 })
 export class ProductModule { }
